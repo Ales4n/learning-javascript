2d060ab23a8db5eea9307b3b2fcd86621ecdb7ae
Alessandro <san07ar@gmail.com>
2020-08-01 20:07:55 +0200

    exercise not finished

---
+++ node/conditionals/numbers_digits.js
@@ -0,0 +1,18 @@
+let n1 = 2346
+let n2 = 5621
+
+n1.toString()
+n2.toString()
+
+
+for(let i=0;i< n ;i++)
+{
+    if(string1[i]==string2[i])
+    {
+      //do something, make a counter that increments here...
+      //also save index i, so you can tell the position when a match occured
+    }else 
+    {
+      //do something else if you need to do something when chars didnt match
+    }
+}
