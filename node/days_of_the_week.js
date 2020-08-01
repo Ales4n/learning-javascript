95bed57aa8ee281139ffde32102fc1cf09f3ce7b
Alessandro <san07ar@gmail.com>
2020-08-01 19:33:33 +0200

    first commit

---
+++ node/days_of_the_week.js
@@ -0,0 +1,22 @@
+let day = 8;
+
+switch(day){
+    case 1: console.log("Monday");
+    break;
+    case 2: console.log("Tuesday");
+    break;
+    case 3: console.log("Wendsday");
+    break;
+    case 4: console.log("Thrusday");
+    break;
+    case 5: console.log("Friday");
+    break;
+    case 6: console.log("Saturday");
+    break;
+    case 7: console.log("Sunday");
+    break;
+
+    default:
+        console.log("Not a day")
+    
+}
