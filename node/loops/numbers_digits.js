606b52546c61867effd82a7f3e386208a203ac14
Alessandro <san07ar@gmail.com>
2020-08-03 22:24:37 +0200

    change file folder

--- node/loops/numbers_digits.js
+++ node/loops/numbers_digits.js
@@ -1,14 +1,15 @@
 let n1 = 2346
-let n2 = 5621
+let n2 = 2346
 
-n1.toString()
-n2.toString()
+let string1 = n1.toString()
+let string2 = n2.toString()
 
-
-for(let i=0;i< n ;i++)
+for(let i=0;i< string1.length ;i++)
 {
     if(string1[i]==string2[i])
     {
+      let counter
+      counter++
       //do something, make a counter that increments here...
       //also save index i, so you can tell the position when a match occured
     }else 
