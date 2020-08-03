5b3359a3ce2c12fbaac390b5c28bb52c4a3cd9bd
Alessandro <san07ar@gmail.com>
2020-08-03 22:25:27 +0200

    add files

---
+++ node/loops/generating_a_number.js
@@ -0,0 +1,7 @@
+let num = 59;
+
+if(num < 100){
+    for(let i= num; i <= 100;i++){
+        console.log(i)
+    }
+}
---
+++ node/loops/potencia.js
@@ -0,0 +1,8 @@
+let num = 10
+
+let result
+
+for(let i = 1; i <= num; i++){
+    result=Math.pow(2, i)
+    console.log(result)
+    }
