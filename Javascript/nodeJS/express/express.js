4e2d1df1590629bae768f8596645ac38abed31a2
Alessandro <san07ar@gmail.com>
2020-08-29 13:04:17 +0200

    add nodemon

--- Javascript/nodeJS/express/express.js
+++ Javascript/nodeJS/express/express.js
@@ -6,7 +6,12 @@ server.get('/',  function (req,res){
     res.send('<h1>HELLO</h1>')
     res.end()
 })
-
+server.get('/about', (req,res)=>{
+    res.send('<h1>About me</h1>')
+})
+server.get('/contact', (req,res)=>{
+    res.send('<h1>Form contact</h1>')
+})
 server.listen(3000, function(){
     console.log('Server on port 3000'.red)
 })
