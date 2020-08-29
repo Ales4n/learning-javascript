8b18466f5d8bae8d7a180b925dffd5c07688fb41
Alessandro <san07ar@gmail.com>
2020-08-29 18:54:45 +0200

    use of the methods of express

--- Javascript/nodeJS/express/express.js
+++ Javascript/nodeJS/express/express.js
@@ -2,16 +2,35 @@ const express = require('express')
 const colors = require('colors')
 const server = express()
 
-server.get('/',  function (req,res){
+server.use(express.json())
+server.use(logger)
+
+function logger(req, res, next){
+    console.log(`Route received: ${req.protocol}://${req.get('host')}${req.originalUrl}`)
+    next()
+}
+
+// server.all('/user', (req, res, next) => {
+//     console.log('passed through here')
+//     next()
+// })
+
+server.get('/user', (req, res) => {
     res.send('<h1>HELLO</h1>')
     res.end()
 })
-server.get('/about', (req,res)=>{
-    res.send('<h1>About me</h1>')
+server.post('/user/:id', (req, res) => {
+    console.log(req.body)
+    console.log(req.params)
+    res.send('<h1>POST REQUEST RECEIVED</h1>')
 })
-server.get('/contact', (req,res)=>{
-    res.send('<h1>Form contact sdasas</h1>')
+server.delete('/user/:id/', (req, res) => {
+    res.send(`User ${req.params.id} deleted`)
 })
-server.listen(3000, function(){
-    console.log('Server on port 3000'.red)
+server.put('/user/:id', (req, res) => {
+    res.send(`User ${req.params.id} updated`)
+    console.log(req.body)
+})
+server.listen(5000, function () {
+    console.log('Server on port 5000'.red)
 })
