8b2baccd9f22fded1576605e8307bbfe3727b1ab
Alessandro <san07ar@gmail.com>
2020-08-30 18:05:52 +0200

    installation and use of EJS methods

--- Javascript/nodeJS/express/express.js
+++ Javascript/nodeJS/express/express.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const colors = require('colors')
+const morgan = require('morgan')
 const server = express()
 
 server.use(express.json())
-server.use(logger)
+server.use(morgan('dev'))
 
-function logger(req, res, next){
-    console.log(`Route received: ${req.protocol}://${req.get('host')}${req.originalUrl}`)
-    next()
-}
+server.set('appName', 'Ale')
+server.set('port', 3000)
+
+// function logger(req, res, next){
+//     console.log(`Route received: ${req.protocol}://${req.get('host')}${req.originalUrl}`)
+//     next()
+// }
 
 // server.all('/user', (req, res, next) => {
 //     console.log('passed through here')
 //     next()
 // })
+
+server.get('/', (req, res) => {
+    const data = [{name: "jose"}, {name: "julian"}, {name: "cheo"}]
+    res.render('index.ejs', {people: data})
+})
 
 server.get('/user', (req, res) => {
     res.send('<h1>HELLO</h1>')
@@ -31,6 +40,9 @@ server.put('/user/:id', (req, res) => {
     res.send(`User ${req.params.id} updated`)
     console.log(req.body)
 })
-server.listen(5000, function () {
-    console.log('Server on port 5000'.red)
+
+server.use(express.static('public'))
+
+server.listen(server.get('port'), function () {
+    console.log('Server on port 3000'.red)
 })
